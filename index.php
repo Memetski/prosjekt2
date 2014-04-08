f12a12d4408937d070c5d5f49366137b3191e4fc
Memetski <memet_cat@hotmail.com>
2014-04-08 16:00:24 +0200

    Opprettet div filer
    Opprettet index.php for prosjekt
    Opprettet logo for prosjekt
    Opprettet stylesheet for endring av side

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+	<html>
+	
+		<head>
+		 <meta charset="utf-8">
+		 <title> Central PUB </title>
+		<link href="project2.css" rel="stylesheet" type="text/css">
+		</head>
+		
+		
+		
+		<body>
+	
+			<div class="left">
+			
+				<img src="logo.png" width="100%" height="100%" alt="Logo">
+				<h1>     </h1>
+			
+			
+			
+			</div>
+		
+			
+			<div class="midt">
+			
+				<h1>    </h1>
+			
+			</div>
+		
+		
+		
+		
+		</body>
+	
+	
+	</html>
