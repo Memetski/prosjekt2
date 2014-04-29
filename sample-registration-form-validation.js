1af4a13ad88a78c78c27b1c2349b5ab5252920ca
Sigurd <eXclusive1337@gmail.com>
2014-04-29 13:40:38 +0200

    Nye filer for opretting av bruker

---
+++ sample-registration-form-validation.js
@@ -0,0 +1,13 @@
+function ValidateEmail(uemail)  
+{  
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(uemail.value.match(mailformat))  
+{  
+return true;  
+}  
+else  
+{  
+alert("You have entered an invalid email address!");  
+uemail.focus();  
+return false;  
+}
