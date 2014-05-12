687019ade0b8d45d7123b4598921f11a53c70120
Memetski <memet_cat@hotmail.com>
2014-05-12 17:11:22 +0200

    loggin ok

--- ErPaalogget.php
+++ ErPaalogget.php
@@ -9,7 +9,7 @@ $sql = 'SELECT * FROM bruker WHERE uname=? AND pwd=?';
 $stm = $db->prepare ($sql);
 $stm->execute (array ($_POST['uname'], md5($_POST['pwd'])));
 if ($row = $stm->fetch()) {
-$_SESSION['user'] = $_POST['uname'];
+$_SESSION['uid'] = $row['uid'];
 echo  json_encode (array("ok"=>"OK"));
 } else
 echo 'No match for username/password';
--- loginok.php
+++ loginok.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 require_once 'db.php';
-$sql = 'SELECT * FROM bruker WHERE uname=?';
+$sql = 'SELECT * FROM bruker WHERE uid=?';
 $sth = $db->prepare ($sql);
-$sth->execute (array ($_SESSION['uname']));
+$sth->execute (array ($_SESSION['uid']));
 if ($row=$sth->fetch()) {?>
 <b>Velkommen<br/>
 	<?php echo $row['fornavn']; ?> <?php echo $row['etternavn']; ?></b><br>
