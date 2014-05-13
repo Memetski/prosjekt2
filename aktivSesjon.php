d89e336236590e58166a4a665bb10ee75f8a4c15
Andersnoem <andersnoem90@gmail.com>
2014-05-13 16:55:16 +0200

    Slettet unødvendige filer, etc

--- aktivSesjon.php
+++ aktivSesjon.php
@@ -4,14 +4,14 @@
     require_once 'db.php';
 	
 	
-    $sql = 'SELECT * FROM bruker WHERE uid=?';
+    $sql = 'SELECT * FROM bruker WHERE uid=?';	// Henter ut brukerID
     $sth = $db->prepare ($sql);
-	if(isset($_SESSION['uid'])) {
+	if(isset($_SESSION['uid'])) {	// Dersom det eksisterer en sesjon, utfør kallet. Ellers henvis til innlogging
     $sth->execute (array ($_SESSION['uid']));
 	}else header("Location:login.html");
 	
 	
-    if ($row=$sth->fetch()) {
+    if ($row=$sth->fetch()) {	// Hvis man lyktes i å hente ut brukerID, returner til Ajax-funksjonen
     echo json_encode (array ('login'=>'OK'));
     } else {
     echo json_encode (array ('logon'=>'NOPE'));
